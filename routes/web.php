b154d8be635615840f711fc6a2f35f3c4969a550
AHMADSEPTIANEKA <154289144+AHMADSEPTIANEKA@users.noreply.github.com>
2025-02-21 19:03:03 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LaptopController;
 use App\Http\Controllers\AccessoryController;
-
+use App\Http\Controllers\SearchController;
+Route::get('/search', [SearchController::class, 'index'])->name('search');
 Route::resource('accessories', AccessoryController::class);
 Route::get('home',  [AccessoryController::class, 'home'])->name('accessories.home');
-
 Route::resource('phones', PhoneController::class);
 Route::resource('laptops', LaptopController::class);
-
 // Route untuk menampilkan ponsel Android
 Route::get('android', [PhoneController::class, 'android'])->name('phones.android');
 Route::get('gaming', [LaptopController::class, 'gaming'])->name('laptops.gaming');
@@ -20,10 +19,8 @@ Route::get('workstation', [LaptopController::class, 'workstation'])->name('lapto
 // Route untuk menampilkan ponsel iPhone
 Route::get('iphone', [PhoneController::class, 'iphone'])->name('phones.iphone');
 Route::get('android', [PhoneController::class, 'android'])->name('phones.android');
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
